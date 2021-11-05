49e60a4687e7e73db365d5fdd2392a61c655ccbc
er.vsachan <er.vsahan@gmail.com>
2021-11-05 21:58:30 +0530

    Add project files.

---
+++ VartikaAppService1234/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VartikaAppService1234.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
